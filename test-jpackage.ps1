5a465047c8b095863bd0d121eaf61311d34adf9c
Izumi <dimaburak428@gmail.com>
2026-05-12 22:43:59 +0300

    chore: remove test artifacts from repo

--- test-jpackage.ps1
+++
@@ -1,7 +0,0 @@
-$ErrorActionPreference = "Stop"
-cd C:\Users\dimab\lost-database
-mvn clean package -DskipTests
-rm -Recurse -Force dist-test -ErrorAction SilentlyContinue
-mkdir dist-test
-cp target\lost-game-database-1.0-SNAPSHOT.jar dist-test\lost.jar
-jpackage --input dist-test --main-jar lost.jar --main-class com.lost.database.app.Launcher --name "LOST-Test" --type app-image --dest out-test
